f270fd5924232e906652f9b6c131490cf0c44ca0
Yojanse <yojanseduarte@gmail.com>
2022-02-20 23:40:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+/* 	Yojanse Duarte 
+	yojanseduarte@gmail.com
+	02-20-2022 
+*/
+
+
+/*This is my fist homework from coursera course*/
---
+++ stats.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.c> 
+ * @brief <here we have some functions to works with arrays >
+ *
+ * <This is my first homework, here i have a few functions to works with arrays like, median,mean,maximum, minimum and ordenice of some numbers. >
+ *
+ * @author <Yojanse Duarte>
+ * @date <02-20-2022 >
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+  /*
+
+*/
+
+}
+
+/* Add other Implementation File Code Here */
