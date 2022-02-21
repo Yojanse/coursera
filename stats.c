a432b9e8998345188f88b86790487a1710983bce
Yojanse <yojanseduarte@gmail.com>
2022-02-21 01:03:31 -0400

    third commit

--- stats.c
+++ stats.c
@@ -37,10 +37,23 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
-
+		unsigned char minimum = 0;
+		unsigned char maximum = 0;
+		float mean = 0;
+		unsigned char median = 0;																		
   /* Statistics and Printing Functions Go Here */
 
-   
+		/* Statistics and Printing Functions Go Here */
+		printf("Array before order: \n");
+		print_array(&test, SIZE);
+		median =  find_median(&test, SIZE);
+		mean =  find_mean(&test, SIZE);
+		maximum =  find_maximum(&test, SIZE);
+		minimum =  find_minimum(&test, SIZE);
+		print_statistics( minimum, maximum,  mean, median);
+		sort_array(&test, SIZE);
+		printf("Array after order: \n");
+		print_array(&test, SIZE);
     
 
 
@@ -49,3 +62,78 @@ void main() {
 /* Add other Implementation File Code Here */
 
 
+void print_statistics (unsigned char minimum, unsigned char maximum, float mean, unsigned char median){
+  printf("The minimum value is %d \n", minimum);
+  printf("The maximum is %d \n", maximum);
+  printf("The mean is %f \n", mean);
+  printf("The median is %d \n", median);
+}
+
+int i;
+void print_array (unsigned char *array, unsigned int counter){
+  for (i=0; i<counter; i++){
+    printf("%d,", *(array + i));
+  }
+  printf("\n");
+}
+
+unsigned char find_median (unsigned char *array, unsigned int counter){
+  unsigned char median = 0;
+  median = *(array + ((counter / 2) -1));
+  return median;
+}
+
+float find_mean (unsigned char *array, unsigned int counter){
+  unsigned int totalizer = 0; 
+  float mean = 0;
+  for ( i=0; i<counter; i++){
+    totalizer = totalizer + array[i] ;
+  }
+  printf("mean is  = %d \n", totalizer);
+  mean = totalizer / ((float) counter); 
+  return mean;
+}
+
+unsigned char find_maximum (unsigned char *array, unsigned int counter){
+  unsigned char maximum = *array; 
+  for ( i=1; i<counter; i++){ 
+    if (*(array + i) > maximum){
+      maximum = *(array + i);
+    }
+  
+  }
+  return maximum;
+}
+
+unsigned char find_minimum (unsigned char *array, unsigned int counter){
+  unsigned char minimum = *array; 
+  for ( i=1; i<counter; i++){ 
+    if (*(array + i) < minimum){
+      minimum = *(array + i);
+    }
+    
+  }
+  return minimum;
+}
+int index;
+void sort_array (unsigned char *array, unsigned int counter){
+  char flag = 0; 
+  unsigned char temp;
+  do {
+    flag =0; 
+    for ( index=0; index<counter; index++){
+      if (array[index] > array[index +1] || array[index] == array[index +1]) {
+        continue;
+      }
+      else if (array[index] < array[index +1]) {
+       
+        temp = array[index];
+        array[index] = array[index+1];
+        array[index+1] = temp;
+
+        flag = 1; 
+      }
+    }
+  }
+  while (flag ==1); 
+}
